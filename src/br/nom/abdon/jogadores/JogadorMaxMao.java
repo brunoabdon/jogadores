905aca9f110196f333b699b870a933e33b9ced8d
Bruno Abdon <brunoabdon+github@gmail.com>
2014-06-23 13:44:07 -0300

    arrumação/formatação

--- src/br/nom/abdon/jogadores/JogadorMaxMao.java
+++ src/br/nom/abdon/jogadores/JogadorMaxMao.java
@@ -21,52 +21,40 @@ public class JogadorMaxMao implements Jogador{
         
     private boolean perguntouSeEuQueriaJogar;
     
+    private Function<Contador, Jogada> joga;
+    
     private final static Comparator<Contador> comp = Comparator.naturalOrder();
     
     @Override
     public void recebeMao(Pedra ... pedras) {
-        this.mao = 
-                EnumSet.of(
-                        pedras[0], 
-                        pedras[1], 
-                        pedras[2], 
-                        pedras[3], 
-                        pedras[4], 
-                        pedras[5]);
+        this.mao = EnumSet.of(
+            pedras[0], pedras[1], pedras[2], pedras[3], pedras[4], pedras[5]
+        );
+        
         this.contador = new Contador(mao);
         
-        perguntouSeEuQueriaJogar = false;
+        this.joga = 
+            c-> {
+                Pedra p = c.getPedra();
+                Lado l = p.temNumero(mesa.getNumeroEsquerda()) 
+                        ? Lado.ESQUERDO 
+                        : Lado.DIREITO;
+                this.contador = c;
+                return joga(p, l);
+            };
         
+        this.perguntouSeEuQueriaJogar = false;
     }
 
     @Override
     public Jogada joga() {
-        
-        final Jogada jogada;
-        
-        if (mesa.taVazia() && !perguntouSeEuQueriaJogar) {
-            jogada = joga(DominoUtil.aMaiorCarroca(mao),Lado.ESQUERDO);
-        } else {
-
-            final Function<Contador, Jogada> joga = 
-                c-> {
-                    Pedra p = c.getPedra();
-                    Lado l = p.temNumero(mesa.getNumeroEsquerda()) 
-                            ? Lado.ESQUERDO 
-                            : Lado.DIREITO;
-                    this.contador = c;
-                    return joga(p, l);
-                };
-            
-            jogada = 
-                this.mao.stream()
+        return mesa.taVazia() && !perguntouSeEuQueriaJogar
+            ? joga(DominoUtil.aMaiorCarroca(mao),Lado.ESQUERDO)
+            : this.mao.stream()
                 .filter(DominoUtil.jogavel(mesa))
                 .map(contador::projete)
                 .min(comp)
                 .map(joga).orElse(Jogada.TOQUE);
-            
-        }
-        return jogada;
     }
 
     @Override
@@ -75,10 +63,9 @@ public class JogadorMaxMao implements Jogador{
         return 5;
     }
 
-    private Jogada joga(Pedra pedra,Lado lado){
-        mao.remove(pedra);
+    private Jogada joga(Pedra pedra, Lado lado){
+        this.mao.remove(pedra);
         return new Jogada(pedra,lado);
-
     }
 
     @Override
